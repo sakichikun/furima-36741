5d070e6cc4ab47bcd5086d771b428525507559ec
sakichikun <sakitty24.xxx@gmail.com>
2021-09-11 12:40:15 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :scheduled_delivery
 end
