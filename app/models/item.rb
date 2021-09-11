eeafa2b4fa27c675051e581eb9886271156f2938
sakichikun <sakitty24.xxx@gmail.com>
2021-09-11 12:55:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   belongs_to :burden
   belongs_to :area
   belongs_to :scheduled_delivery
+
+  belongs_to :user
+  has_one :purchase, dependent: :destroy
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :burden_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, presence: true
 end
