89e988508fbdd38fffc64d8210a12e56875f8dbe
sakichikun <sakitty24.xxx@gmail.com>
2021-09-11 13:09:44 +0900

    画像ファイルの紐付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :purchase, dependent: :destroy
+  has_one_attached :image
 
   validates :name, presence: true
   validates :text, presence: true
