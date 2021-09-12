f481a9875d7ee8968d96b700a123903ca11f0b3a
sakichikun <sakitty24.xxx@gmail.com>
2021-09-12 23:11:40 +0900

    url遷移による編集権限の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all
     @items = Item.order("created_at DESC")
@@ -39,6 +41,13 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :burden_id, :area_id, :scheduled_delivery_id, :price, :image).merge(user_id:current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
 end
