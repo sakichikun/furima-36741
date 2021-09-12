fd40a59f414bc667117f01bb08f1476f28d654ac
sakichikun <sakitty24.xxx@gmail.com>
2021-09-12 18:33:33 +0900

    コントローラーでshowアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :burden_id, :area_id, :scheduled_delivery_id, :price, :image).merge(user_id:current_user.id)
